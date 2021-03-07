3a6d7682ed2231a2535d8cddf49e85f711addd36
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-03-07 04:06:55 -0300

    Main, Carousel (Incomplete)

---
+++ Bootcamp_HTML/Netflix/assets/js/scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  feather.replace();
+  
+  let slider = tns({
+    items: 1,
+    touch: true,
+    controls: false,
+    nav: false,
+
+    responsive: {  
+      576: {
+        items: 2,
+        gutter: 8,
+        controls: true,
+        controlsText: ['<i data-feather="chevron-left"></i>', '<i data-feather="chevron-right"></i>']
+      },
+
+      768: {
+        items: 3,
+      }
+    }
+  });
+});
