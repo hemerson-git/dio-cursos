4bef6c1689a6139258b0ceebd46fe576fd597918
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-03-10 02:14:48 -0300

    Animation

--- Bootcamp_HTML/Netflix/assets/js/scripts.js
+++ Bootcamp_HTML/Netflix/assets/js/scripts.js
@@ -1,8 +1,35 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
+  startCarousel();
+  feather.replace();
+
+  document.addEventListener('scroll', changeNavbarBg);
+
+});
+
+function changeNavbarBg() {
+  const menu = document.querySelector('#main-menu');
+  
+  if (pageYOffset === 0) {
+    menu.classList.add('translucent');
+  } else {
+    menu.classList.remove('translucent');
+  }
+}
+
+function toggleMenu() {
+  const menu = document.querySelector('#main-menu');
+
+}
+
+function startCarousel() {
   let slider = tns({
     items: 1,
     touch: true,
-    controls: false,
+    controls: true,
+    controlsText: ['<i data-feather="chevron-left"></i>', '<i data-feather="chevron-right"></i>'],
+    rewind: false,
     nav: false,
 
     responsive: {  
@@ -10,7 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
         items: 2,
         gutter: 8,
         controls: true,
-        controlsText: ['<i data-feather="chevron-left"></i>', '<i data-feather="chevron-right"></i>']
       },
 
       768: {
@@ -18,6 +44,4 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
-
-  feather.replace();
-});
+}
