3632eff4f3f0f7e977922f7c14af38e010b12bd0
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-03-17 23:44:21 -0300

    snake move

--- Bootcamp_HTML/Snake/assets/js/scripts.js
+++ Bootcamp_HTML/Snake/assets/js/scripts.js
@@ -1,10 +1,57 @@
 let canvas = document.querySelector('#snake');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
-function criarBG() {
+let direction = "right";
+
+function createBackground() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function createSnake() {
+  for(let i = 0; i < snake.length; i++) {
+    context.fillStyle = 'green';
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function gameStart() {
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction === 'right') {
+    snakeX += box;
+  }
+
+  if (direction === 'left') {
+    snakeX -= box;
+  }
+
+  if (direction === 'up') {
+    snakeY += box;
+  }
+
+  if (direction === 'down') {
+    snakeY -= box;
+  }
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(gameStart, 100)
