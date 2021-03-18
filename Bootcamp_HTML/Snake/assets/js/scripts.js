7c1cef2f37adafe07857e5de8a7c276633861c67
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-03-18 00:12:37 -0300

    set loop

--- Bootcamp_HTML/Snake/assets/js/scripts.js
+++ Bootcamp_HTML/Snake/assets/js/scripts.js
@@ -15,13 +15,36 @@ function createBackground() {
 }
 
 function createSnake() {
-  for(let i = 0; i < snake.length; i++) {
+  for (let i = 0; i < snake.length; i++) {
     context.fillStyle = 'green';
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
+document.addEventListener('keydown', chageDirection);
+
+function chageDirection(e) {
+  // Pega o código do evento e faz um replace para deixar apenas a string de direção
+  let newDirection = (e.code).replace('Arrow', '').toLowerCase();
+
+  if (
+    (direction === 'up' && newDirection === 'down') ||
+    (direction === 'down' && newDirection === 'up') ||
+    (direction === 'left' && newDirection === 'right') ||
+    (direction === 'right' && newDirection === 'left')
+  ) {
+    return;
+  }
+  
+  direction = newDirection;
+}
+
 function gameStart() {
+  if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+  if(snake[0].x < 0 && direction === 'left') snake[0].x = 15 * box;
+  if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+  if(snake[0].y < 0 && direction === 'up') snake[0].y = 15 * box;
+  
   createBackground();
   createSnake();
 
@@ -36,11 +59,11 @@ function gameStart() {
     snakeX -= box;
   }
 
-  if (direction === 'up') {
+  if (direction === 'down') {
     snakeY += box;
   }
 
-  if (direction === 'down') {
+  if (direction === 'up') {
     snakeY -= box;
   }
 
