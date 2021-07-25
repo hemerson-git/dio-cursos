cb981e9561d1f132e337f40be6ce951feb63162e
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-07-24 22:32:41 -0300

    Added Redux

--- Bootcamp_Localiza_react/restaurant_finder_with_google_maps/restaurant-finder/src/App.tsx
+++ Bootcamp_Localiza_react/restaurant_finder_with_google_maps/restaurant-finder/src/App.tsx
@@ -1,15 +1,20 @@
 import { ThemeProvider } from "styled-components";
-import theme from "./themes/default-theme";
+import { Provider } from "react-redux";
 
+import store from "./redux/store";
+
+import theme from "./themes/default-theme";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Home />
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Home />
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
