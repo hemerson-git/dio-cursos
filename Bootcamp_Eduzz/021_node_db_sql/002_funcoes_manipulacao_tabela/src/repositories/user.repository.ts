f04704d0927ed22b96a4116876a033f811d8741d
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-12-20 22:51:48 -0300

    020 - 002 - remove, update User

--- Bootcamp_Eduzz/021_node_db_sql/002_funcoes_manipulacao_tabela/src/repositories/user.repository.ts
+++ Bootcamp_Eduzz/021_node_db_sql/002_funcoes_manipulacao_tabela/src/repositories/user.repository.ts
@@ -48,6 +48,38 @@ class UserRepository {
 
     return newUser.uuid;
   }
+
+  async update(user: User): Promise<string> {
+    const script = `
+      UPDATE application_user 
+      SET
+        username = $1,
+        password = crypt($2, gen_salt('md5'))
+      WHERE uuid = $3
+    `;
+
+    const { username, password } = user;
+
+    const values = [username, password, user.uuid];
+
+    const { rows } = await db.query<{ uuid: string }>(script, values);
+
+    const [updatedUser] = rows;
+
+    return updatedUser.uuid;
+  }
+
+  async remove(uuid: string): Promise<void> {
+    const script = `
+      DELETE 
+      FROM application_user
+      WHERE uuid = $1
+    `;
+
+    const values = [uuid];
+
+    await db.query(script, values);
+  }
 }
 
 export default new UserRepository();
